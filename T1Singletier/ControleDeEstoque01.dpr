e04fa999a7dabc10dccee56c8f41a4836eaac64d
Diayne <diaynebeatto23@gmail.com>
2016-08-16 16:30:34 -0300

    Trabalho Diayne Beatto

---
+++ T1Singletier/ControleDeEstoque01.dpr
@@ -0,0 +1,14 @@
+program ControleDeEstoque01;
+
+uses
+  Vcl.Forms,
+  ControleDeEstoque in 'ControleDeEstoque.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
